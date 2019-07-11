9defd0467de80574ac8026fc477bff7d9bae0761
Vu Khuu <vu.khuu@vsource.io>
2019-07-11 23:05:15 +1200

    [Implemented] Script to install

---
+++ install.sh
@@ -0,0 +1,7 @@
+composer install
+mysql -hdb -uroot -padmin -e "CREATE DATABASE laravel_blog COLLATE 'utf8_unicode_ci';"
+mysql -hdb -uroot -padmin -e "CREATE DATABASE laravel_blog_testing COLLATE 'utf8_unicode_ci';"
+php artisan migrate
+composer dump-autoload
+php artisan db:seed
+php artisan db:seed --class=UsersTableSeeder
